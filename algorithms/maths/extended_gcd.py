a1afedd61ed80350aff9de7f89b3ce568d26d7c1
CharNoir <59160057+CharNoir@users.noreply.github.com>
2024-05-10 19:37:04 +0200

    Added unit tests for math

--- algorithms/maths/extended_gcd.py
+++ algorithms/maths/extended_gcd.py
@@ -11,10 +11,10 @@ def extended_gcd(num1, num2):
     """
     
     if num1 != 0 and num2 == 0:
-        return (1, 0, num2) 
+        return (1, 0, num1) 
     
     if num1 == 0:
-        return (0, 1, 0) 
+        return (0, 1, num2) 
     
     old_s, s = 1, 0
     old_t, t = 0, 1
